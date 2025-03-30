f42eac6aeff91da7091212465d0494397eef1a78
Henry <obijrnwagwu@gmail.com>
2025-03-30 13:46:36 -0500

    in chat we trust

---
+++ frontend/components/BuyHouse.tsx
@@ -0,0 +1,11 @@
+const BuyHouse = () => {
+  return (
+    <div>
+      <button className="bg-blue-500 text-white px-4 py-2 rounded-lg hover:bg-blue-600">
+        Buy House
+        </button>
+    </div>
+  );
+}
+
+export default BuyHouse;
